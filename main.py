71711ac7bdbc2ee7e80746bd20292230d2cf17ce
slyd0x <github@slydox.de>
2025-04-02 22:31:34 +0200

    added function to count each character in the string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_each_char
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,4 +8,7 @@ def get_book_text(filepath):
 
 def main():
     get_num_words(get_book_text("books/frankenstein.txt"))
+    num_of_chars = get_each_char(get_book_text("books/frankenstein.txt"))
+    for char in num_of_chars:
+        print(f"'{char}': {num_of_chars[char]}")
 main()    
--- stats.py
+++ stats.py
@@ -3,3 +3,12 @@ def get_num_words(text):
     num_words = len(words)
     print(f"{num_words} words found in the document")
 
+def get_each_char(text):
+    text = text.lower()
+    result = {}
+    for i in range(0, len(text)):
+        if text[i] not in result:
+            result[text[i]] = 1
+        else:
+            result[text[i]] += 1
+    return result
